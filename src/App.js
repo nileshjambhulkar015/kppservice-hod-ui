0876cb6bc789316a3a69c2f190fe6b83a59dce4c
nileshjambhulkar015 <nileshjambhulkar@gmail.com>
2023-11-22 16:25:14 +0530

    cookies deleted code added

--- src/App.js
+++ src/App.js
@@ -9,15 +9,28 @@ import Cookies from 'js-cookie';
 
 
 function App() {
-  const eid = Cookies.get('empEId');
-  console.log(eid)
+  //remove cookies when click on logout
+  const removeCookies = () => {
+    Cookies.remove('empId');
+    Cookies.remove('roleId');
+    Cookies.remove('roleName');
+    Cookies.remove('deptId');
+    Cookies.remove('deptName');
+    Cookies.remove('desigId');
+    Cookies.remove('desigName');
+    Cookies.remove('empEId');
+    Cookies.remove('empFirstName');
+    Cookies.remove('empMiddleName');
+    Cookies.remove('empLastName');
+  }
+
   return (
   
       <BrowserRouter>
       <nav className="navbar navbar-inverse">
         <div className="container-fluid">
           <div className="navbar-header">
-            <a className="navbar-brand" href="http://localhost:3008">FutureBizops</a>
+            <a className="navbar-brand" href="http://localhost:3008"  onClick={() => removeCookies()}>FutureBizops</a>
           </div>
           <ul className="nav navbar-nav">      
            
@@ -28,7 +41,7 @@ function App() {
           </ul>
           <ul className="nav navbar-nav navbar-right">
           <li><a href="#">Welcome: {Cookies.get('empEId')}</a></li>
-            <li><a href="http://localhost:3008">Logout</a></li>
+            <li><a href="http://localhost:3008"  onClick={() => removeCookies()}>Logout</a></li>
           </ul>
         </div>
       </nav>

45116cbb2c74916dccaad4f89a773c23e9df0c41
nileshjambhulkar015 <nileshjambhulkar@gmail.com>
2023-12-09 23:39:35 +0530

    KPP api integration

--- src/components/EmployeeKppComponent/EmployeeKppComponent.js
+++ src/components/EmployeeKppComponent/EmployeeKppComponent.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Cookies from 'js-cookie';
 import EmployeeKppService from '../../services/EmployeeKppService';
 
 
-export default function EmployeeKppComponent(){
-    
-
+export default function EmployeeKppComponent() {
+    const [kppEmpStatus, setKppEmpStatus] = useState('Male');
+    const [remark, setRemark] = useState('');
     const [kppResponses, setKppResponses] = useState([])
-
+    const [todos, setTodos] = useState([{ kppId: "", empId: "", empEId: "", roleId: "", deptId: "", desigId: "", ekppAchivedWeight: "", ekppOverallAchieve: "", ekppOverallTaskComp: "", ekppAppliedDate: "", ekppEvidence: "", ekppStatus: "Pending" }]);
     useEffect(() => {
         EmployeeKppService.getKPPDetails().then((res) => {
             setKppResponses(res.data);
-           
+
         });
     }, []);
 
-    return(
+    const handleTodoChange = (e, i, kppId, kppOverallTarget) => {
+        const field = e.target.name;
+        const newTodos = [...todos];
+
+        newTodos[i] = {
+            ...newTodos[i],
+            "kppId": kppId,
+            "empId": Cookies.get('empId'),
+            "empEId": Cookies.get('empEId'),
+            "roleId": Cookies.get('roleId'),
+            "deptId": Cookies.get('deptId'),
+            "desigId": Cookies.get('desigId'),
+            "ekppStatus": "Pending",  // NEED TO MAKE IT DYNAMIC
+            "ekppOverallTaskComp": field === "ekppOverallAchieve" && !!e.target.value ? Number(e.target.value) + Number(kppOverallTarget) : 0,
+            "ekppAchivedWeight": field === "ekppOverallAchieve" && !!e.target.value ? Number(e.target.value) + Number(kppOverallTarget) : 0,
+            [field]: e.target.value || 0,
+        }
+        setTodos(newTodos);
+    };
+
+    const saveEmployeeKpp = (e) => {
+        e.preventDefault()
+
+        EmployeeKppService.saveEmployeeKppDetails(todos).then(res => {
+            console.log("KPP added");
+        }
+        );
+    }
+
+    return (
         <div className='container-fluid'>
             <div className="row">
                 <form className="form-horizontal">
@@ -29,67 +59,88 @@ export default function EmployeeKppComponent(){
                                 <th>Overall Target</th>
                                 <th>Target Period</th>
                                 <th>UOM</th>
-
                                 <th>Achived Weightage</th>
                                 <th>Over All Achive</th>
                                 <th>Overall Task Completed</th>
                                 <th>Overall Weightage</th>
-                                <th>View Evidence</th>
-                                <th>Hod Ratings</th>
-                               
-                          
+                                <th>Achived Weightage</th>
+                                <th>Over All Achive</th>
+                                <th>Overall Task Completed</th>
                             </tr>
                         </thead>
                         <tbody>
-                       
+
                             {
-                                 
+
                                 kppResponses.map(
-                                    
+
                                     (kppResponse, index) =>
                                         <tr key={kppResponse.kppId} className="text-justify">
 
-                                            <td>{index+1}</td>
+                                            <td>{index + 1}</td>
                                             <td className='col-md-2'>{kppResponse.kppObjective}</td>
                                             <td>{kppResponse.kppPerformanceIndi}</td>
                                             <td>{kppResponse.kppOverallTarget}</td>
                                             <td>{kppResponse.kppTargetPeriod}</td>
                                             <td>{kppResponse.kppUoM}</td>
-                                           <td>{kppResponse.ekppAchivedWeight}</td>
-                                       <td>{kppResponse.ekppOverallAchieve}</td>
-                                       <td>{kppResponse.ekppOverallTaskComp}</td>
-                                      
-                                        
+                                            <td>{kppResponse.ekppAchivedWeight}</td>
+                                            <td>{kppResponse.ekppOverallAchieve}</td>
+                                            <td>{kppResponse.ekppOverallTaskComp}</td>
                                             <td>{kppResponse.kppOverallWeightage}</td>
-                                            <td>{kppResponse.ekppEvidence}</td>
+
                                             <td>
-                                            <input type="text" className="form-control" id="hodRatings" placeholder="HOD Ratings"/>
-                                       </td>
-                                           
+                                                <input type="text" className="form-control" name="ekppAchivedWeight" defaultValue={0} value={todos[index]?.ekppAchivedWeight} disabled />
+                                            </td>
+                                            <td>
+                                                <input type="number" className="form-control" name="ekppOverallAchieve" defaultValue={0} onChange={event => handleTodoChange(event, index, kppResponse.kppId, kppResponse.kppOverallTarget)} />
+                                            </td>
+                                            <td>
+                                                <input type="text" className="form-control" name="ekppOverallTaskComp" defaultValue={0} value={todos[index]?.ekppOverallTaskComp} disabled />
+                                            </td>
                                         </tr>
                                 )
-                               
 
                             }
-                          
                         </tbody>
                     </table>
+                    <div className="form-group">
+                        <label className="control-label col-sm-4" htmlFor="reamrk">Upload Evidence:</label>
+                        <div className="col-sm-3">
+                            <input type="file" className="form-control" id="deptName" />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label col-sm-4" htmlFor="reamrk">Enter Remark:</label>
+                        <div className="col-sm-6">
+                            <textarea row="5" className="form-control" id="remark" placeholder="Enter Remark here" value={remark} onChange={(e) => setRemark(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                                    <div className="row">
+                                        <label className="control-label col-sm-4" htmlFor="kppEmpStatus">Status:</label>
+                                        <div className="col-sm-3">
+                                            <select className="form-control" id="kppEmpStatus" onChange={(e) => setKppEmpStatus(e.target.value)} value={kppEmpStatus} >
+                                                <option value={'Approve'}>Approve</option>
+                                                <option value={'Reject'}>Reject</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                </form>
 
-                    </form>
-                    
-                </div>
-               <div className="row">
+            </div>
+            <div className="row">
                 <div className="col-sm-8"></div>
-                <div className="col-sm-4"><button type="submit" className="btn btn-success col-sm-offset-1" > Accept</button>
-                <button type="submit" className="btn btn-danger col-sm-offset-1 "> Reject</button>
-                <button type="submit" className="btn btn-primary col-sm-offset-1"> Cancle</button>
-               
+                <div className="col-sm-4"><button type="submit" className="btn btn-success col-sm-offset-1" > Submit</button>
+                    <button type="submit" className="btn btn-info col-sm-offset-1 "> Back</button>
+                 
+
                 </div>
-               </div>
-               <div className="row">
-             <h4>  *Note - Please refere the below table for ratings:</h4>
+            </div>
+            <div className="row">
+                <h4>  *Note - Please refere the below table for ratings:</h4>
                 <div className="col-sm-5">
-                               <table className="table table-bordered">
+                    <table className="table table-bordered">
                         <thead>
                             <tr className="text-center">
                                 <th>Sr No</th>
@@ -98,19 +149,19 @@ export default function EmployeeKppComponent(){
                                 <th>Rating 2</th>
                                 <th>Rating 3</th>
                                 <th>Rating 4</th>
-                                <th>Rating 5</th>                          
+                                <th>Rating 5</th>
                             </tr>
                         </thead>
                         <tbody>
-                       
+
                             {
-                                 
+
                                 kppResponses.map(
-                                    (kppResponse,index) =>
+                                    (kppResponse, index) =>
 
                                         <tr className="text-center">
 
-                                            <td>{index+1}</td>
+                                            <td>{index + 1}</td>
                                             <td className="text-justify">{kppResponse.kppObjective}</td>
                                             <td>{kppResponse.kppRating1}</td>
                                             <td>{kppResponse.kppRating2}</td>
@@ -119,15 +170,15 @@ export default function EmployeeKppComponent(){
                                             <td>{kppResponse.kppRating5}</td>
                                         </tr>
                                 )
-                               
+
 
                             }
-                          
+
                         </tbody>
-                        
+
                     </table>
-</div></div>       </div>
- 
+                </div></div>       </div>
+
 
     );
 }
--- src/services/EmployeeKppService.js
+++ src/services/EmployeeKppService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
+import { KPP_API_BASE_URL, LOGIN_UI_BASE_URL } from "./EmployeeConstants";
 const BASE_URL = "http://localhost:9091/hod-approval/employee-kpp?empId=1&statusCd=A";
 
 class EmployeeKppService {
@@ -14,6 +15,17 @@ class EmployeeKppService {
         }
 
     }
+
+    saveEmployeeKppDetails(todos){
+     
+        if (null != Cookies.get('empId')) {
+            return axios.put(KPP_API_BASE_URL+"/employee-key-perform-parameter",todos)
+        } else {
+            alert("You need to login first")
+            window.location.replace(LOGIN_UI_BASE_URL);
+        }  
+    }
+
 }
 
 
